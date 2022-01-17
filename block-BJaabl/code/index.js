10ff56e38be0e0d01e0a82f25e82145c8fc42dd8
achyuth yadav <achyuthyadav@hmail.com>
2022-01-17 10:42:13 +0530

    assignment2

--- block-BJaabl/code/index.js
+++ block-BJaabl/code/index.js
@@ -1,6 +1,11 @@
 /*
 1. Create a function named `sayHello` that that accepts a parameter `name` and alert `Hello [name]!`.
 */
+function sayHello(name){
+  alert(`hello ${name}`)
+}
+sayHello(`achyuth`);
+
 
 /*
 2. Create a function named `getFullName` that accepts two parameter `firstName` and `lastName` and returns
@@ -11,6 +16,13 @@ Example:
   getFullName("Nelson", "Mandela"); // "Nelson Mandela"
 */
 
+function getFullName(firstName , lastName){
+  return firstName + lastName
+}
+getFullName("john" , "snow")
+
+
+
 /*
 3. Create a function named `addTwoNumbers` that accepts two numbers i.e `firstNum` and `secondNum` and returns
 the sum of both numbers. If user passes any data other then numbers alert a message saying `Enter Valid Input`
@@ -20,6 +32,12 @@ addTwoNumbers(10, 22); // 32
 addTwoNumbers(20, 32); // 32
 addTwoNumbers(10, "100"); // Alert Enter Valid Input
 */
+
+function addTwoNumbers(firstNum, secondNum){
+  return firstNum + secondNum
+}
+addTwoNumbers(10, 22)
+
 
 /*
 4. Create a function named `calc` which accepts three parameter `numA`, `numB` and `operation`. Operation
@@ -32,6 +50,12 @@ calc(10, 20, 'add'); // 30
 calc(20, 10, 'sub'); // 10
 calc(20, 10, 'mul'); // 200
 */
+function calc(numA, numB, operation ){
+  return( )
+  alert(`enter valid input`)
+}
+
+
 
 
 /*
@@ -42,7 +66,16 @@ isLeapYear(2000); // true
 isLeapYear(2001); // false
 */
 
+function isLeapYear(){
+  
+}
+
+
 
 /*
 6. Create a function named `getFactorial` that accepts a number and return the factorial of the number.
 */
+
+function getFactorial(){
+  return()
+}
